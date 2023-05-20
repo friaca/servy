60ea8037d68e4ef03d000fd7c164666c90a03588
friaca <friacajoao@gmail.com>
2023-05-19 21:45:59 -0300

    refact: `rewrite_path` to be more ergonomic using regex

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -18,15 +18,17 @@ defmodule Servy.Handler do
 
   def emojify(conv), do: conv
 
-  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
-    %{conv | path: "bears/#{id}"}
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
   end
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings" }
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{ conv | path: "/#{thing}/#{id}" }
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path_captures(conv, nil), do: conv
 
   def track(%{status: 404, path: path} = conv) do
     IO.puts("Warning: #{path} is on the loose!")
