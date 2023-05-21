ce3fe81c09e7313743aefa92fc5688a8bb6bfff0
friaca <friacajoao@gmail.com>
2023-05-21 18:26:10 -0300

    feat: creating Conv struct and migrating methods to use it

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -9,6 +9,8 @@ defmodule Servy.Handler do
   import Servy.FileHandler
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1, emojify: 1]
 
+  alias Servy.Conv
+
   @doc """
   Transforms the request into a respons
   """
@@ -23,50 +25,50 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%{method: "GET", path: "/wildthings"} = conv) do
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{method: "GET", path: "/bears/new"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
     @pages_path
     |> Path.join("form.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
+  def route(%Conv{method: "DELETE", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id} deleted!"}
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
     @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/pages/" <> page} = conv) do
+  def route(%Conv{method: "GET", path: "/pages/" <> page} = conv) do
     @pages_path
     |> Path.join("#{page}.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{path: path} = conv) do
+  def route(%Conv{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
@@ -74,16 +76,7 @@ defmodule Servy.Handler do
     """
   end
 
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
-  end
+
 end
 
 request = """
--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -1,40 +1,42 @@
 defmodule Servy.Plugins do
   import Logger
 
-  def log(conv), do: IO.inspect(conv)
+  alias Servy.Conv
 
-  def emojify(%{status: 200} = conv) do
+  def log(%Conv{} = conv), do: IO.inspect(conv)
+
+  def emojify(%Conv{status: 200} = conv) do
     %{conv | resp_body: "🎺 #{conv.resp_body} 🎺"}
   end
 
   def emojify(conv), do: conv
 
-  def rewrite_path(%{path: path} = conv) do
+  def rewrite_path(%Conv{path: path} = conv) do
     regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
     captures = Regex.named_captures(regex, path)
     rewrite_path_captures(conv, captures)
   end
 
-  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+  def rewrite_path_captures(%Conv{} = conv, %{"thing" => thing, "id" => id}) do
     %{ conv | path: "/#{thing}/#{id}" }
   end
 
-  def rewrite_path_captures(conv, nil), do: conv
+  def rewrite_path_captures(%Conv{} = conv, nil), do: conv
 
-  def track(%{status: 404, path: path} = conv) do
+  def track(%Conv{status: 404, path: path} = conv) do
     warn("Warning: #{path} is on the loose!")
     conv
   end
 
-  def track(%{status: 200, path: path} = conv) do
+  def track(%Conv{status: 200, path: path} = conv) do
     info("#{path} completed successfully")
     conv
   end
 
-  def track(%{status: 500, path: path} = conv) do
+  def track(%Conv{status: 500, path: path} = conv) do
     error("#{path} crashed!")
     conv
   end
 
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 end
