88ee02d819dcc7af1dac303bbd5a664b5b065f24
friaca <friacajoao@gmail.com>
2023-05-21 17:41:16 -0300

    feat: creating pages_path constant

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,6 +1,15 @@
 defmodule Servy.Handler do
+  @moduledoc """
+  Handles HTTP requests.
+  """
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
   import Logger
 
+  @doc """
+  Transforms the request into a response
+  """
   def handle(request) do
     request
     |> parse
@@ -72,7 +81,7 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/bears/new"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("form.html")
     |> File.read()
     |> handle_file(conv)
@@ -83,14 +92,14 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
   end
 
   def route(%{method: "GET", path: "/pages/" <> page} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("#{page}.html")
     |> File.read()
     |> handle_file(conv)
