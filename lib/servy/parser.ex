7b194f61872e72f1153d6d89c16b9f8941b8c8f0
friaca <friacajoao@gmail.com>
2023-05-22 22:18:25 -0300

    feat: adding support for header parsing in the request

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -5,12 +5,29 @@ defmodule Servy.Parser do
     [top, params_string] = String.split(request, "\r\n\r\n")
     [request_line | header_lines] = String.split(top, "\r\n")
     [method, path, _] = String.split(request_line, " ")
-    params = parse_params(params_string)
+    headers = parse_headers(header_lines, %{})
+    params = parse_params(headers["Content-Type"], params_string)
 
-    %Conv{ method: method, path: path, params: params }
+    %Conv{
+      method: method,
+      path: path,
+      params: params,
+      headers: headers
+    }
   end
 
-  def parse_params(params_string) do
+  def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim |> URI.decode_query()
   end
+
+  def parse_params(_content_type, _params_string), do: %{}
+
+  def parse_headers([head | tail], headers) do
+    [key, value] = String.split(head, ": ")
+
+    headers = Map.put(headers, key, value)
+    parse_headers(tail, headers)
+  end
+
+  def parse_headers([], headers), do: headers
 end
