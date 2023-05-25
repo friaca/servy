2589386ce8b6844dd0c39c5cd775a7f2fc147dcd
friaca <friacajoao@gmail.com>
2023-05-25 00:36:25 -0300

    feat(exercise): map function implementation

--- lib/recurse.ex
+++ lib/recurse.ex
@@ -14,7 +14,14 @@ defmodule Recurse do
   end
 
   def triple([]), do: []
+
+  def my_map([head | tail], func) do
+    [func.(head) | my_map(tail, func)]
+  end
+
+  def my_map([], _func), do: []
 end
 
 IO.puts Recurse.sum([1, 2, 3, 4, 5])
 IO.inspect Recurse.triple([1, 2, 3, 4, 5])
+IO.inspect Recurse.my_map([1, 2, 3], fn x -> x * 2 end)
