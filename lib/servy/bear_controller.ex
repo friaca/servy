1839ef9ed69fdc10aa9c07a324bdf809a07e077c
friaca <friacajoao@gmail.com>
2023-05-26 00:38:20 -0300

    feat: rendering templates

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,25 +1,22 @@
 defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
+  alias Servy.BearView
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
-  end
+  @templates_path Path.expand("templates", File.cwd!)
 
   def index(conv) do
     bears =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join()
 
-    %{conv | status: 200, resp_body: "<ul>#{bears}</ul>"}
+    %{conv | status: 200, resp_body: BearView.index(bears)}
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
+
+    %{conv | status: 200, resp_body: BearView.show(bear)}
   end
 
   def create(conv, %{"name" => name, "type" => type} = _params) do
