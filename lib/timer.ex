e643a360b02da447fa1f180cfe0c9512d7671811
friaca <friacajoao@gmail.com>
2023-06-04 18:22:51 -0300

    feat(exercise): `Timer` module

---
+++ lib/timer.ex
@@ -0,0 +1,14 @@
+defmodule Timer do
+  def remind(name, time_seconds) do
+    spawn(fn ->
+      :timer.sleep(time_seconds * 1000)
+      IO.puts(name)
+    end)
+  end
+end
+
+Timer.remind("Stand Up", 5)
+Timer.remind("Sit Down", 10)
+Timer.remind("Fight, Fight, Fight", 15)
+
+:timer.sleep(:infinity)
