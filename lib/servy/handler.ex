460ff79bd3dd74c3dcc683aa2870cec99ad19f88
friaca <friacajoao@gmail.com>
2023-07-01 20:52:13 -0300

    feat: /snapshots route

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,6 +7,7 @@ defmodule Servy.Handler do
 
   import Servy.Parser
   import Servy.FileHandler
+  import Servy.VideoCam
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
 
   alias Servy.Conv
@@ -24,6 +25,22 @@ defmodule Servy.Handler do
     |> track
     |> put_content_length
     |> format_response
+  end
+
+  def route(%Conv{ method: "GET", path: "/snapshots" } = conv) do
+    caller = self()
+
+    spawn(fn -> send(caller, {:result, get_snapshot("cam-1")}) end)
+    spawn(fn -> send(caller, {:result, get_snapshot("cam-2")}) end)
+    spawn(fn -> send(caller, {:result, get_snapshot("cam-3")}) end)
+
+    snapshot1 = receive do {:result, filename} -> filename end
+    snapshot2 = receive do {:result, filename} -> filename end
+    snapshot3 = receive do {:result, filename} -> filename end
+
+    snapshots = [snapshot1, snapshot2, snapshot3]
+
+    %{ conv | status: 200, resp_body: inspect snapshots}
   end
 
   def route(%Conv{ method: "GET", path: "/hibernate/" <> time } = conv) do
