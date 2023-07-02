4ddf7a84f0ea1bcb43b7ee579c110a4e07dd86a7
friaca <friacajoao@gmail.com>
2023-07-01 22:56:29 -0300

    feat(exercise): testing concurrent requests

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -11,4 +11,27 @@ defmodule HttpServerTest do
     assert response.status_code == 200
     assert response.body == "Bears, Lions, Tigers"
   end
+
+  test "accepts multiple requests and returns the responses correctly" do
+    port = 4000
+    parent = self()
+    num_requests = 5
+
+    spawn(Servy.HttpServer, :start, [port])
+
+    for _ <- 1..num_requests do
+      spawn(fn ->
+        {:ok, response} = HTTPoison.get("http://localhost:#{port}/wildthings")
+        send(parent, {:ok, response})
+      end)
+    end
+
+    for _ <- 1..num_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
+  end
 end
