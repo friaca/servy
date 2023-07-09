f751552931946bc5e2eb38bbc55deb2e8059fd64
friaca <friacajoao@gmail.com>
2023-07-09 17:19:30 -0300

    feat: :total_pledged message handling

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -2,11 +2,33 @@ defmodule Servy.PledgeServer do
 
   @name :pledge_server
 
+  # Client interface
+
   def start() do
     pid = spawn(__MODULE__, :listen_loop, [[]])
     Process.register(pid, @name)
     pid
   end
+
+  def create_pledge(name, amount) do
+    send(@name, {self(), :create_pledge, name, amount})
+
+    receive do {:response, id} -> id end
+  end
+
+  def recent_pledges() do
+    send(@name, {self(), :recent_pledges})
+
+    receive do {:response, pledges} -> pledges end
+  end
+
+  def total_pledged() do
+    send(@name, {self(), :total_pledged})
+
+    receive do {:response, total} -> total end
+  end
+
+  # Server
 
   def listen_loop(state) do
     receive do
@@ -21,19 +43,18 @@ defmodule Servy.PledgeServer do
       {sender, :recent_pledges} ->
         send(sender, {:response, state})
         listen_loop(state)
+
+      # This doesn't return the actual total pledged,
+      # just the sum of the most recent three pledges
+      {sender, :total_pledged} ->
+        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum()
+        send(sender, {:response, total})
+        listen_loop(state)
+
+      unexpected ->
+        IO.puts("Unexpected message: #{inspect(unexpected)}")
+        listen_loop(state)
       end
-  end
-
-  def create_pledge(name, amount) do
-    send(@name, {self(), :create_pledge, name, amount})
-
-    receive do {:response, id} -> id end
-  end
-
-  def recent_pledges() do
-    send(@name, {self(), :recent_pledges})
-
-    receive do {:response, pledges} -> pledges end
   end
 
   defp send_pledge_to_service(_name, _amount) do
@@ -53,3 +74,4 @@ end
 # IO.inspect(PledgeServer.create_pledge("grace", 50))
 
 # IO.inspect(PledgeServer.recent_pledges())
+# IO.inspect(PledgeServer.total_pledged())
